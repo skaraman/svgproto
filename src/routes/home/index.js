bf03c475da93f3c6a1da59469324ef58c7e3fcdd
Semyon Karaman <semyonkaraman@Semyons-MBP.rws.ad.ea.com>
2019-04-09 18:25:02 -0700

    three

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,22 +1,60 @@
-import { h, Component } from 'preact';
-import style from './style';
+import { h, Component } from 'preact'
+import style from './style'
 
-import Terminal from 'components/terminal';
+import Terminal from 'components/terminal'
 
-import Box from '!!preact-svg-loader!svg/test/box.svg';
-import svgson, { stringify } from 'svgson';
+import Box from '!!preact-svg-loader!svg/test/box.svg'
+import svgson, { stringify } from 'svgson'
 
 export default class Home extends Component {
-
-    componentDidMount() {
-        this.box;
-        debugger
+    constructor(props) {
+        super(props)
+        this.svgsToRender = {
+            box: true,
+            star: true
+        }
+        console.log(Box({}))
     }
 
+    componentWillMount() {
+        console.log('componentWillMount');
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount');
+    }
+
+    componentWillUnmount() {
+        console.log('componentWillUnmount');
+    }
+
+    componentWillReceiveProps() {
+        console.log('componentWillReceiveProps');
+    }
+
+    shouldComponentUpdate() {
+        console.log('shouldComponentUpdate');
+    }
+
+    componentWillUpdate() {
+        console.log('componentWillUpdate');
+    }
+
+    componentDidUpdate() {
+        console.log('componentDidUpdate');
+    }
+
+    //
+    // shouldComponentUpdate() {
+    //     for (let svgdx in this.svgsToRender) {
+    //         let svg = this[svgdx]
+    //         if (!svg) return false
+    //     }
+    //     return true
+    // }
+
     render({}, {}) {
-        console.log(
-            svgson(Box)
-        );
+
         return (
             <div class={style.home}>
                 <p>There should be a SVG Box...</p>
@@ -24,6 +62,6 @@ export default class Home extends Component {
 
                 <Terminal />
             </div>
-        );
+        )
     }
 }
